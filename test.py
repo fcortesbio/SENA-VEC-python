0805a27ec63d2a1629f434c6b7d275d2dafae6ef
Andres <fcortesbio@gmail.com>
2024-03-12 14:23:12 -0500

    Added test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+# Challenge 1 
+# Author : Fabián Andrés Cortés Tróchez
+# Date: 2024 - 03 - 12
+
+weapons = [".", "-", "+", "*", "T", "Y", "|", "W", "X", "M"]
+
+def select_weapon()->str:
+    weapon_set = input()
+    
